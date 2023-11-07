328a52e553032dca24d0545f9c724ec7615cbb3b
klarkxy <278370456@qq.com>
2023-11-07 23:02:52 +0800

    增加自动打蛋机MOD

---
+++ AutoEggCracker/AutoEggCracker.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Klarkxy
+{
+    public class AutoEggCracker
+	{
+		[HarmonyPatch(typeof(EggCrackerConfig), "CreateBuildingDef")]
+		public class EggCrackerConfig_CreateBuildingDef
+		{
+			public static bool Prepare()
+			{
+				return true;
+			}
+
+			public static void Postfix(BuildingDef __result)
+			{
+				// 增加信号端口
+				__result.LogicInputPorts = LogicOperationalController.CreateSingleInputPortList(new CellOffset(0, 0));
+			}
+		}
+	}
+}
